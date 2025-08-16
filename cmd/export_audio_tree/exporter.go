47731215be9d9ceef41703dc7f1c4862ff40d974
Terry M. Poulin <BigBoss1964@gmail.com>
2025-08-16 15:23:15 +0000

    Exporter is now asyncronous.
    This separates the fs.WalkDir into two separate walks:
    
    1. Creating each directory.
    2. Copying and converting each file.
    
    With the second walk functioning by adding the operations to a work pool.
    
    Further, the ffmpeg run is now performed in the background and sent the the log (if any).
    
    In general, this takes the test case of converting my library to FLAC from a runtime of 30-40 minutes down to 5-10 minutes when running on my MacBook Air 2022 (M2 / 16 GB). Consumes most of the RAM, but running 8 concurrent flac conversions is pretty darn quick and actually takes advantage of these cores :).

--- cmd/export_audio_tree/exporter.go
+++ cmd/export_audio_tree/exporter.go
@@ -13,11 +13,13 @@ import (
 	"io/fs"
 	"os"
 	"path/filepath"
+	"time"
 )
 
 type Exporter struct {
 	ctx     context.Context
 	opts    *options.ExporterOptions
+	pool    *WorkPool
 	InRoot  filesystem.FS
 	OutRoot filesystem.FS
 }
@@ -26,6 +28,7 @@ func newExporter(ctx context.Context, opts *options.ExporterOptions) *Exporter {
 	return &Exporter{
 		ctx:     ctx,
 		opts:    opts,
+		pool:    NewWorkPool(ctx, opts.MaxJobs, opts.MaxQueue),
 		InRoot:  filesystem.NewFileSystem(opts.InRoot),
 		OutRoot: filesystem.NewFileSystem(opts.OutRoot),
 	}
@@ -33,9 +36,38 @@ func newExporter(ctx context.Context, opts *options.ExporterOptions) *Exporter {
 
 // Make the magic happen, or return the error code.
 func (p *Exporter) Run() error {
-	return fs.WalkDir(p.InRoot, ".", p.walkFunc)
+	// First execute WalkDir to ensure that all directories are created. This
+	// will allow us to run the remaining tasks asyncronously without having
+	// data races over "hey, I was just about to create that directory."
+	if err := fs.WalkDir(p.InRoot, ".", p.visitDir); err != nil {
+		return err
+	}
+
+	// Spin up the work pool.
+	p.pool.Start()
+
+	// Periodically log the status of the pool.
+	go func() {
+		for {
+			time.Sleep(time.Second * 30)
+			logging.Printf("WorkPool %p: size: %d limit: %d buffer: %d (%f %%)",
+				p.pool, p.pool.Size(), p.pool.Limit(), p.pool.Remaining(), p.pool.PercentFull())
+		}
+	}()
+
+	// Now execute WalkDir to feed the beast. This will block until all items are in the queue, which may require blocking until
+	if err := fs.WalkDir(p.InRoot, ".", p.visitFile); err != nil {
+		return err
+	}
+
+	// Now wait for everyone to finish.
+	p.pool.Wait()
+
+	return nil
 }
 
+// Walk function for creating directories in the output root.
+//
 // Called with <path> <base name of dir if its a dir> <err>
 //
 // E.g., if there is ${root}/artist/album/song, this would be called the
@@ -51,11 +83,38 @@ func (p *Exporter) Run() error {
 // return fs.SkipAll to abort the entire walk.
 //
 // return any other non-nil error to abort and return the error code.
-func (p *Exporter) walkFunc(path string, d fs.DirEntry, err error) error {
-	logging.Verbosef("Visiting path: %q d.Name: %q err: %v", path, d, err)
+func (p *Exporter) visitDir(path string, d fs.DirEntry, err error) error {
+	logging.Printf("Visiting path: %q d.Name: %q err: %v", path, d, err)
+
+	if !d.IsDir() || path == "." {
+		return nil
+	}
+
+	// We can't count on d.Type().Perm() to be populated by fs.WalkDir, we
+	// need to do a stat of our own.
+	st, err := p.InRoot.Stat(path)
+	if err != nil {
+		return fmt.Errorf("stat failed: %w", err)
+	}
+	logging.Printf("Mkdirs %q", path)
+	return p.OutRoot.MkDirAll(path, st.Mode().Perm())
+}
+
+// Walk function for converting files.
+//
+// See walkDir for a description of how the parameters will be populated by fs.WalkDir().
+//
+// Since directories are created by the initial walk, we only need concern
+// ourselves with valid files. These are either copied or converted as
+// appropriate.
+func (p *Exporter) visitFile(path string, d fs.DirEntry, err error) error {
+	logging.Printf("Visiting path: %q d.Name: %q err: %v", path, d, err)
 
 	// Handle exclusions.
-	if path == "." {
+	if d.IsDir() {
+		// Created by the initial walk using visitDir().
+		return nil
+	} else if path == "." {
 		// We don't care about the root itself.
 		return nil
 	} else if filesystem.IsTrashFile(path) {
@@ -63,27 +122,24 @@ func (p *Exporter) walkFunc(path string, d fs.DirEntry, err error) error {
 		return nil
 	}
 
-	// Handle exports.
-	if d.IsDir() {
-		// We can't count on d.Type().Perm() to be populated by fs.WalkDir, we
-		// need to do a stat. So no point in passing it here.
-		return p.Mkdirs(path)
-	} else if ffmpeg.IsMediaFile(path) {
-		return p.Convert(path)
+	if ffmpeg.IsMediaFile(path) {
+		// Add the conversion to the queue.
+		p.pool.Add(func() {
+			if output, err := p.Convert(path); err != nil {
+				logging.Fatalf("!!! FATAL: %v !!!\n=== Start Output %q ===\n%s\n=== End Output %q ===\n", err, path, output, path)
+			} else {
+				logging.Printf("=== Start Output %q ===\n%s\n=== End Output %q ===\n", path, output, path)
+			}
+		})
 	} else if p.opts.CopyUnknown && !d.IsDir() {
-		return p.Copy(path)
+		// Add copying the file to the queue.
+		p.pool.Add(func() {
+			if err := p.Copy(path); err != nil {
+				logging.Fatalln(err)
+			}
+		})
 	}
 	return nil
-}
-
-// Handle creating the directories defined by path.
-func (p *Exporter) Mkdirs(path string) error {
-	logging.Printf("Mkdirs %q", path)
-	st, err := p.InRoot.Stat(path)
-	if err != nil {
-		return fmt.Errorf("stat failed: %w", err)
-	}
-	return p.OutRoot.MkDirAll(path, st.Mode().Perm())
 }
 
 // Handle copying path between roots. If no clobber is set, we silently ignore
@@ -103,22 +159,29 @@ func (p *Exporter) Copy(path string) error {
 	return err
 }
 
-func (p *Exporter) Convert(path string) error {
+func (p *Exporter) Convert(path string) (string, error) {
 	oldExt := filepath.Ext(path)
 	newExt := "." + p.opts.Format
 
 	if oldExt == newExt {
 		logging.Println(path, "already in target format")
-		return p.Copy(path)
+		return "", p.Copy(path)
 	}
 
 	copts := ffmpeg.GetDefaultOptions(newExt)
 	if copts.Err != nil {
-		return copts.Err
+		return "", copts.Err
 	}
 	copts.InputFile = filepath.Join(p.opts.InRoot, path)
 	copts.OutputFile = filepath.Join(p.opts.OutRoot, path[:len(path)-len(oldExt)]) + newExt
 
 	logging.Verbosef("Converting %q -> %q", copts.InputFile, copts.OutputFile)
-	return ffmpeg.Convert(p.ctx, copts)
+	output, err := ffmpeg.ConvertInBackground(p.ctx, copts)
+	if err != nil {
+		if output == nil {
+			output = []byte{}
+		}
+		return string(output), fmt.Errorf("converting %q failed with error: %v", copts.InputFile, err)
+	}
+	return string(output), err
 }
---
+++ cmd/export_audio_tree/workpool.go
@@ -0,0 +1,192 @@
+// SPDX-License-Identifier: Zlib
+// Copyright 2025, Terry M. Poulin.
+package main
+
+import (
+	"context"
+	"runtime"
+	"sync"
+)
+
+// Defines a work pool for executing callbacks.
+//
+// Work to be done is defined by a simple function, which will execute on the
+// next available worker. If the queue is full, a batch of additional goroutines
+// will be created up to the defined limit.
+//
+// Unlike a channel and infinite goroutines, this places a limit on how many can
+// conversions can concurrently exist. The point is to convert audio, and memory
+// nor process cycles are infinite when users are doing other work at the same
+// time. Likewise, slower I/O devices such as memory cards will choke under
+// heavy concurrent I/O.
+//
+// Unlike a channel and a fixed number of goroutines, this allows some measure
+// of dynamic scaling. A low limit can restrict resource usage during export. A
+// high limit can ramp up more concurrent exports if you're willing to dedicated
+// excessive resources, but don't always export such a large collection.
+type WorkPool struct {
+	ctx    context.Context    // Used for shutdown of the pool.
+	cancel context.CancelFunc // Used for shutdown of the pool.
+	buffer int                // Buffer size for queue.
+	size   int                // Number of goroutines in the pool.
+	limit  int                // Max value for size.
+	wg     sync.WaitGroup     // Used for shutdown of the pool.
+	mutex  sync.Mutex         // Protects the size field.
+	queue  chan func()        // Channel of tasks for the goroutines.
+}
+
+// Creates a new work pool. Call Start() to spawn the initial workers and use
+// Add() to push callbacks onto the work queue for execution.
+//
+// A maximum number of goroutines can be specified with [limit]. If 0, the limit
+// is [runtime.NumCPU].
+//
+// The queue size will be set to [buffer], or a default value if 0 was provided.
+func NewWorkPool(parent context.Context, limit int, buffer int) *WorkPool {
+	ctx, cancel := context.WithCancel(parent)
+	if limit == 0 {
+		limit = runtime.NumCPU()
+	}
+	if buffer == 0 {
+		buffer = max(limit, 100)
+	}
+	return &WorkPool{
+		ctx:    ctx,
+		cancel: cancel,
+		limit:  limit,
+		buffer: buffer,
+		queue:  make(chan func(), buffer),
+	}
+}
+
+// Spawns a set of workers. Up to [runtime.NumCPU] or the pool limit will be
+// created. Additional goroutines will be generated as necessary up to the
+// limit.
+func (p *WorkPool) Start() {
+	p.mutex.Lock()
+	defer p.mutex.Unlock()
+
+	if p.size == 0 {
+		// Restarting queue after a stop. We do this here rather than in stop,
+		// because this makes Add() panic on send to a closed channel if Start
+		// hasn't been called. If we created the new channel before Stop exists,
+		// deadlock could occur by calling Add on a stopped pool until the
+		// channel buffer is full.
+		p.queue = make(chan func(), p.buffer)
+	}
+
+	ncpu := runtime.NumCPU()
+	for i := 0; i < ncpu && i < p.limit; i++ {
+		p.wg.Add(1)
+		p.size++
+		go p.worker()
+	}
+}
+
+// Stop all workers and abort tasks remaining in the queue. This will block
+// until everyone finishes their current item and halts. If the parent context
+// is closed, this occurs automatically. After calling this returns, you must
+// call [Start] before it is possible to add any items to the queue.
+func (p *WorkPool) Stop() {
+	// Acquire the mutex to prevent anyone calling Add().
+	p.mutex.Lock()
+	defer p.mutex.Unlock()
+
+	// Halt the workers at their next tick.
+	p.cancel()
+	p.wg.Wait()
+	p.size = 0
+
+	// There may be items remaining in the queue. To ensure they're subject to
+	// GC, they or the queue must go. Since nil'ing the queue would be a data
+	// race with concurrent Adds (not that you should be doing that, if you
+	// called Stop :), we close and drain the queue.
+	close(p.queue)
+	for range p.queue {
+		// We don't want to execute, just ensure the channel doesn't retain the
+		// memory.
+	}
+}
+
+// Drain the queue and halt all workers. This can be used to wait for the
+// completion of queued callbacks without After this returns, you must call
+// [Start] before adding any further items to the queue.
+func (p *WorkPool) Wait() {
+	// Workers will halt once the queue drains.
+	close(p.queue)
+	p.wg.Wait()
+}
+
+// Add a callback to the work queue. If the queue is full, additional goroutines
+// will be spawned up to the limit. By default, the queue is
+func (p *WorkPool) Add(fn func()) {
+	p.expand()
+	p.queue <- fn
+}
+
+// Possibly expands the work pool. Up to 4 workers are created if the queue is
+// full, provided the limit has not been reached.
+func (p *WorkPool) expand() {
+	p.mutex.Lock()
+	defer p.mutex.Unlock()
+	if p.size == p.limit {
+		// Pool can't grow any further.
+		return
+	}
+
+	if p.Remaining() > 0 {
+		return
+	}
+
+	// Up to the limit, or this many new go routines.
+	growth := min(4, p.limit-p.size)
+
+	for range growth {
+		p.wg.Add(1)
+		p.size++
+		go p.worker()
+	}
+}
+
+// Returns the approximate amount of queue space remaining.
+func (p *WorkPool) Remaining() int {
+	return cap(p.queue) - len(p.queue)
+}
+
+// Returns a percentage of how full the queue is.
+//
+// Like Remaining, but expressed as a percentage of usage rather than the count
+// of available slots. E.g., "6.0" means the queue is 6% full but Remaining()
+// might be saying 94 slots out of a 100 are free.
+func (p *WorkPool) PercentFull() float64 {
+	return float64(len(p.queue)) / float64(cap(p.queue)) * 100.0
+}
+
+// Return the current size of the work pool.
+func (p *WorkPool) Size() int {
+	p.mutex.Lock()
+	defer p.mutex.Unlock()
+	return p.size
+}
+
+// Returns the maximum number of workers allowed.
+func (p *WorkPool) Limit() int {
+	return p.limit
+}
+
+func (p *WorkPool) worker() {
+	defer p.wg.Done()
+	for {
+		select {
+		case <-p.ctx.Done():
+			// Pool is shutting down.
+			return
+		case fn, ok := <-p.queue:
+			if !ok {
+				// The queue is closed.
+				return
+			}
+			fn()
+		}
+	}
+}
