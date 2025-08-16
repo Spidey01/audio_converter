dd199d58266e07ad3b6c7d438dec83ba39a057da
Terry M. Poulin <BigBoss1964@gmail.com>
2025-08-16 01:20:30 +0000

    Add test for IsTrashFile.

--- internal/filesystem/fs_test.go
+++ internal/filesystem/fs_test.go
@@ -13,3 +13,15 @@ func TestFileSystem(t *testing.T) {
 		t.Fatal(err)
 	}
 }
+
+func TestIsTrashfile(t *testing.T) {
+	if !IsTrashFile(".DS_Store") || !IsTrashFile("/foo/bar/.DS_Store") {
+		t.Errorf("Failed to catch finder info file.")
+	}
+	if !IsTrashFile("._DS_Store") || !IsTrashFile("/foo/bar/.DS_Store") || !IsTrashFile("/foo/bar/._file.ext") {
+		t.Errorf("Failed to catch Apple Double file.")
+	}
+	if IsTrashFile(".hidden") {
+		t.Errorf("Failed caught Unix hidden file, which isn't trash")
+	}
+}
